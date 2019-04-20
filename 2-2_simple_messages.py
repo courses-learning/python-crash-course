60175d9cc6b78f58d85acb89f1a75457bce48647
David Weatherspoon <david_weatherspoon@hotmail.com>
2019-04-20 15:54:19 +0100

    More git and github practice also test of atom and python version setup using f-strings

---
+++ 2-2_simple_messages.py
@@ -0,0 +1,12 @@
+""" Store a message in a variable, and then print that message.
+Then change the value of your variable to a new message, and re-print"""
+
+
+def message(name):
+    print(f"Hello {name} nice to meet you!!\n")
+
+
+name = "David"
+message(name)
+name = "Daniel"
+message(name)
