a53f6e2e6e37200d5da86381cae44a41495f0da6
David Weatherspoon <david_weatherspoon@hotmail.com>
2019-04-21 13:01:40 +0100

    Lists revision with adding, commit & push practice on git & github.

---
+++ 3-4_guest_list.py
@@ -0,0 +1,4 @@
+# Make a list of 3 people you would invite to dinner & send personalised invite
+guests = ['bradley wiggins', 'albert einstein', 'winston churchill']
+for guest in guests:
+    print(f"{guest.title()} you are invited to dinner at 1800hrs 24 Apr 2019.")
