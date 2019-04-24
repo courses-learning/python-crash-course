aff56d21bf92e7ba70a627e9e321d62d5beceaa3
David Weatherspoon <david_weatherspoon@hotmail.com>
2019-04-24 23:30:44 +0100

    Done for the night hopefully more time tomorrow.

---
+++ 4-2_animals.py
@@ -0,0 +1,5 @@
+# Store 3x animals with something in common in a list and then print
+animals = ['elephant', 'rhino', 'hippo']
+for animal in animals:
+    print(f"{animal.title()}s are very large animals.")
+print("None of the above would make suitable pets!!")
