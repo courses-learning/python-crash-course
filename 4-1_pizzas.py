e502cea5f710b8588c02760008a7012725bef79d
David Weatherspoon <david_weatherspoon@hotmail.com>
2019-04-24 23:22:33 +0100

    SHort of time tonight - long day!!! Couple of easy quick exercises for muscle memory...

---
+++ 4-1_pizzas.py
@@ -0,0 +1,5 @@
+# Make a list of 3x types of pizza and use a for loop to print
+pizzas = ['peperoni', 'hawian', 'meat feast']
+for pizza in pizzas:
+    print(f"I like {pizza} pizza.")
+print('I like pizza takeaway nearly as much as Indian!!!')
