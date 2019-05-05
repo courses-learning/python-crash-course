fecd7c592f85e5fc7fe33686882f23fa1564e777
David Weatherspoon <david_weatherspoon@hotmail.com>
2019-05-05 18:56:05 +0100

    2nd alien added using if & else.

---
+++ 5-4_alien_colours2.py
@@ -0,0 +1,11 @@
+# Same as 5-3 with if else chain
+
+score = 0
+alien1 = "yellow"
+if alien1 == "green":
+    score += 5
+    print('You scored 5 points')
+else:
+    score += 10
+    print('You scored 10 points')
+print(f'Score: {score}')
