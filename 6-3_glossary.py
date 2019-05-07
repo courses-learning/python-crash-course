ecc39f566c2a69ac3cf27690db71844a5c338cc7
David Weatherspoon <david_weatherspoon@hotmail.com>
2019-05-07 21:26:34 +0100

    Revision notes - add to dictionary forgotton concepts.

---
+++ 6-3_glossary.py
@@ -0,0 +1,7 @@
+# Use a dictionary to make a glossary of programming words learnt & meaning
+must_remember = {
+    ".items()": "use after 'for key, value in' loop to iterate over dictionary keys & values"
+}
+
+for key, value in must_remember.items():
+    print(f"{key} : {value}")
