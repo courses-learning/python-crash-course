7ce84bfb656a660c57cc3751415e019f94f42e94
David Weatherspoon <david_weatherspoon@hotmail.com>
2019-05-07 21:11:40 +0100

    Dictionary refresher.

---
+++ 6-1_person.py
@@ -0,0 +1,12 @@
+# Use a dict to store info about a person then print each bit of info
+a0001 = {
+    "first": "daniel",
+    "last": "smith",
+    "age": 25,
+    "city": "durham",
+}
+
+print(a0001["first"].title(), end=" ")
+print(a0001["last"].title())
+print(a0001["age"])
+print(a0001["city"].title())
