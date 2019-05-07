b328b5074a84d41784842dcfb87830b4b4791aa3
David Weatherspoon <david_weatherspoon@hotmail.com>
2019-05-07 21:18:54 +0100

    Refresher looping through dictionarys using for key, value in var.items() - need to remember to use .items().

---
+++ 6-2_favourite_numbers.py
@@ -0,0 +1,9 @@
+fav_colours = {
+    'david': 'green',
+    'nicola': 'orange',
+    'joe': 'red',
+    'luke': 'blue',
+}
+
+for key, value in fav_colours.items():
+    print(f"{key.title()}'s favourite colour is {value}")
