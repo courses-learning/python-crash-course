c81cf69213c5ec44ff4c022a214a36c8bb9f4e1d
David Weatherspoon <david_weatherspoon@hotmail.com>
2019-05-14 21:45:33 +0100

    Fixed something deleted by accident.

--- 7-1_rental_car.py
+++ 7-1_rental_car.py
@@ -1,4 +1,4 @@
 # Ask user kind of rental car they want & print as sentance.
 
 car_req = input("What type of rental car would you like? ")
-print(f"Let's see if we can find you a {car_req.title")
+print(f"Let's see if we can find you a {car_req.title}")
