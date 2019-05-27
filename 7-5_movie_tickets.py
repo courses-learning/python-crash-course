5c0fa363c08e2871f562bcab2431519e4b4e9b58
David Weatherspoon <david_weatherspoon@hotmail.com>
2019-05-27 13:11:34 +0100

    While loops to determine if user input is numeric and function used to return movie price based on user inputted age for cinema visitors.

---
+++ 7-5_movie_tickets.py
@@ -0,0 +1,30 @@
+# Write loop asking movie goers age & make ticket price dependant upon this
+
+
+# Func takes an int as age returning int for cost of ticket
+def get_cost(age):
+    if age < 3:
+        return 0
+    elif age < 13:
+        return 10
+    else:
+        return 15
+
+
+no_tickets = ''
+total = 0
+
+# Check input is a number and get no of req tickets
+while no_tickets.isdigit() is False:
+    no_tickets = input('How many tickets would you like? ')
+
+# get age for each ticket, check no is inputted and keep running total
+for i in range(0, int(no_tickets)):
+    age = ''
+    while age.isdigit() is False:
+        age = input(f'What age is ticket {i+1}: ')
+    cost = get_cost(int(age))
+    print(f'This ticket will cost £{cost}')
+    total += cost
+
+print(f'Total cost of all tickets: £{total}')
